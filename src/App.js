95ac8d1bc2102ef6f6e652f6cbf5d969bc95e969
Ananda Surya <99squadd@gmail.com>
2025-05-31 23:04:46 +0700

    Initial commit frontend

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RecommendationUI from './components/RecommendationUI';
+
+function App() {
+  return (
+    <div className="App">
+      <RecommendationUI />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  darkMode: 'class', // Tambahkan ini
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
